1044320043271fd11f7f6f4fbda5f226dc7255ff
BinyamMamo <binyammamo01@gmail.com>
2024-07-10 15:54:13 +0300

    feat(upload): add progress tracking and file details display
    - Add middleware to track upload progress and log to console
    - Update upload handling to display progress bar and file details
    - Add toast notifications for upload success/failure
    - Refactor drag-and-drop handling to support multiple drop zones
    - Update modal UI with artist/music name inputs and file upload sections

--- js/upload.js
+++ js/upload.js
@@ -1,50 +1,191 @@
 $(document).ready(function () {
-  console.clear();
-  $('#file-upload').css('opacity', '0');
+  $('input[name="artistName"]').on('input', function () {
+    const value = $(this).val();
+    const output = $('.artist-music-name span');
+    output[0].innerHTML = value;
+    if (value == '') output[0].innerHTML = 'Artist Name';
+  });
 
-  $('#file-browser').click(function (e) {
+  $('input[name="musicName"]').on('input', function () {
+    const value = $(this).val();
+    const output = $('.artist-music-name span');
+    output[1].innerHTML = value;
+    if (value == '') output[1].innerHTML = 'Music Name';
+  });
+
+  $('.drag-drop').each(function (index, element) {
+    $(element)
+      .find('a')
+      .click(function () {
+        $(this).next('input').trigger('click');
+      });
+  });
+
+  $('.thumbnail').click(function (e) {
     e.preventDefault();
-    $('#file-upload').trigger('click');
+    $('#thumbnail-upload').trigger('click');
   });
 
-  var dropZone = document.getElementById('drop_zone');
+  $('#thumbnail-upload').on('cancel', () => {
+    console.log('Cancelled.');
+  });
 
-  dropZone.addEventListener('dragover', (event) => {
+  $('#thumbnail-upload').on('change', function () {
+    const formData = new FormData();
+    console.log('this:', this);
+    formData.append('file', this.files[0]);
+
+    // Use AJAX to send the file to the server
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/upload', true);
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Handle server response (if needed)
+        console.log('Upload complete!');
+        let response = JSON.parse(xhr.response);
+        $('.thumbnail').css(
+          'background-image',
+          `url('/uploads/${response.filename}')`
+        );
+        toastSuccess('Thumbnail uploaded successfully!');
+      } else if (xhr.readyState == 4) {
+        let response = JSON.parse(xhr.response);
+        toastDanger(response.error);
+      }
+    };
+
+    xhr.send(formData);
+  });
+
+  $('.drop-browse').on('cancel', () => {
+    console.log('Cancelled.');
+  });
+
+  $('.drop-browse').on('change', function () {
+    console.log('this:', this);
+    let element = $(this).parent().parent();
+
+    uploadFile(this.files[0], element[0]);
+  });
+
+  var dropZone = $('.drop-zone');
+
+  dropZone.on('dragover', function (event) {
     event.preventDefault();
-    dropZone.classList.add('hover');
+    this.classList.add('hover');
   });
 
-  dropZone.addEventListener('dragleave', (event) => {
+  dropZone.on('dragleave', function (event) {
     event.preventDefault();
-    dropZone.classList.remove('hover');
+    this.classList.remove('hover');
   });
 
-  dropZone.addEventListener('drop', (event) => {
-    event.preventDefault();
-    dropZone.classList.remove('hover');
-    var files = event.dataTransfer.files;
-    handlesFiles(files);
+  // dropZone.on('drop', function (event) {
+  $.each(dropZone, function (index, element) {
+    element.addEventListener('drop', function (event) {
+      event.preventDefault();
+      element.classList.remove('hover');
+      console.log('event:', event);
+      var files = event.dataTransfer.files;
+      handlesFiles(files, element);
+    });
   });
 
-  function handlesFiles(files) {
-    for (var count = 0; count < files.length; count++) {
-      var file = files[count];
-      uploadFile(file);
-    }
+  function handlesFiles(files, element) {
+    if (files.length > 1) {
+      toastDanger('Upload one file only');
+    } else {
+			uploadFile(files[0], element);
+		}
+
+    // for (var count = 0; count < files.length; count++) {
+    //   var file = files[count];
+    // }
   }
 
-  function uploadFile(file) {
+  function uploadFile(file, element) {
     var formData = new FormData();
     formData.append('file', file);
-    fetch('/upload', {
-      method: 'POST',
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const gallery = document.getElementById('uploadedImage');
-        let html = `<img src="/uploads/${data.filename}" class="img-thumbnail" />`;
-        gallery.innerHTML = gallery.innerHTML + html;
-      });
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/upload', true);
+
+    // Track progress
+    xhr.upload.onprogress = (event) => {
+      if (event.lengthComputable) {
+        const percentComplete = (event.loaded / event.total) * 100;
+        handleProgress(element, percentComplete);
+      }
+    };
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Handle server response (if needed)
+        console.log('Upload complete!');
+        let response = JSON.parse(xhr.response);
+				console.log('response:', response);
+        handleFileDetails(element, response);
+      } else if (xhr.readyState == 4) {
+        let response = JSON.parse(xhr.response);
+        toastDanger(response.error);
+      }
+    };
+
+    xhr.send(formData);
+  }
+
+  function handleProgress(element, percentComplete) {
+    if ($(element).is('.thumbnail')) {
+      console.log('yes it is');
+    } else {
+      const progressContainer = $(element).find('.progress-container');
+      const progressBar = $(element).find('.progress-bar');
+      const percentDisplay = $(element).find('.progress-text');
+
+      progressContainer.show();
+      progressBar.css('width', `${percentComplete}%`);
+      percentDisplay.html(`${Math.round(percentComplete)}%`);
+    }
   }
 });
+
+function handleFileDetails(element, file) {
+  if ($(element).is('.thumbnail')) {
+    $('.thumbnail').css(
+      'background-image',
+      `url('/${file.path}')`
+    );
+    toastSuccess('Thumbnail uploaded successfully!');
+  } else {
+    if ($(element).is('.video-drop'))
+      toastSuccess('Video uploaded successfully!');
+    else if ($(element).is('.lyrics-drop'))
+      toastSuccess('Lyrics uploaded successfully!');
+    else toastSuccess('File uploaded successfully!');
+
+    const fileDetails = $(element).find('.file-details');
+    const fileName = $(element).find('.file-name');
+    const progressContainer = $(element).find('.progress-container');
+    const closeBtn = $(element).find('button.close');
+
+    closeBtn.css('color', 'red');
+    closeBtn.click(function (e) {
+      e.preventDefault();
+      let icon = $(element).find('i');
+      $(icon[0]).show();
+      progressContainer.hide();
+      fileDetails.hide();
+			toastInfo('Removed upload');
+    });
+
+    let icon = $(element).find('i');
+    $(icon[0]).hide();
+    progressContainer.hide();
+    $(element).find('.progress-bar').css('width', '0%');
+    fileDetails.show();
+    fileName.html(`${file.originalname}`);
+		
+		console.log('file:', file);
+  }
+}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 app.use(express.static(__dirname));
 
 // Set up Multer storage
@@ -21,12 +21,48 @@ const storage = multer.diskStorage({
 // Initialize Multer upload
 const upload = multer({ storage: storage });
 
-app.get('/upload', async (request, response) => {
-  response.sendFile(__dirname + '/upload.html');
+// Middleware to track upload progress
+app.use((req, res, next) => {
+  let uploadedBytes = 0;
+  req.on('data', (chunk) => {
+    uploadedBytes += chunk.length;
+    const progress = (uploadedBytes / req.headers['content-length']) * 100;
+    console.log(`Upload progress: ${progress.toFixed(2)}%`);
+  });
+  next();
 });
 
-app.post('/upload', upload.single('file'), (request, response) => {
-  response.json({ filename: request.file.filename });
+app.get('/upload', async (request, response) => {
+  response.sendFile(__dirname + '/upload.html');
+  // response.sendFile(__dirname + '/upload.html');
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  try {
+    // Validate file type (ensure it's an image)
+    // if (!req.file) {
+    //   return res.status(400).json({ error: 'No file uploaded.' });
+    // }
+
+    // // Check if the file is an image (you can use more robust checks here)
+    // const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+    // if (!allowedMimeTypes.includes(req.file.mimetype)) {
+    //   return res.status(400).json({ error: 'Invalid file type. Only images are allowed.' });
+    // }
+
+    // Process the file (e.g., save it to disk or perform further actions)
+    // ...
+
+    // Respond with success message
+    // res.status(200).json({ filename: req.file.filename });
+		console.log('req.file:', req.file);
+    res
+      .status(200)
+      .json(req.file);
+  } catch (error) {
+    console.error('Error handling file upload:', error);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
 });
 
 app.listen(PORT, () => {
