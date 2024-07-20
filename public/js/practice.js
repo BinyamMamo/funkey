c1d84767a6f9a01a98cfae55ad996729be8292b1
BinyamMamo <binyammamo01@gmail.com>
2024-07-20 12:55:52 +0300

    feat(practice): Improve text rendering and cursor handling
    - Add support for newline characters
    - Adjust cursor visibility for newline characters
    - Enhance whitespace rendering
    - Add 'break' class for newline characters
    - Remove unnecessary console.log statement
    - Deleted the following gif files, due to large size:
    	- Crystal Parallax.gif
    	- Flow Field.gif
    	- Sea Waves.gif
    - Added the following audio files:
    	- horror.mp3
    	- jazz.mp3
    	- motivation.mp3
    	- purity.mp3
    - Added the following svg file:
    	- sparkles.svg

--- public/js/practice.js
+++ public/js/practice.js
@@ -11,62 +11,266 @@ But the magic backfired. The carriage turned into a rickety cart, the gown into 
 Despondent, Elara returned the backpack to the attic, vowing to never use its magic again. But she knew she had learned a valuable lesson: true magic lies in kindness and generosity, not in personal gain. From then on, Elara continued to help others, using her own strength and compassion, and her heart, finally free from the burden of magical desires, found true happiness.
 `;
 let cursor = 0;
-const WHITESPACEs = ["·", "&nbsp;", "_"];
+const WHITESPACEs = ['·', '&nbsp;', '_'];
 const WHITESPACE = WHITESPACEs[1];
 
-
 window.addEventListener('load', () => {
-  // const ajax = new XMLHttpRequest();
-  // ajax.onreadystatechange = function() {
-  //   if (this.readyState == 4 && this.status == 200) {
-  //     text = this.response;
-  //     text = text.trim().replace(/\n/g, " ");
-  //     console.log("text: ", text);
-  //     fillTextBox();
-  //   }
-  // }
-  
-  // ajax.open("GET", "text.txt", true);
-  // ajax.send();
-	text = text.trim().replace(/\n/g, " ");
-  fillTextBox();
+  beginPractice();
 });
 
+function beginPractice() {
+  cursor = 0;
+  text = text.trim().replace(/\n+/g, '\n');
+  // text = text.replace(/\n/g, ' ');
+  text = text.replace(/  +/g, ' ');
+  text = text.replace(/\n /g, '\n');
+  text = text.replace(/ \n/g, '\n');
+  fillTextBox();
+}
 
 function fillTextBox() {
-  document.getElementById("textbox").innerHTML = "";
+  document.getElementById('textbox').innerHTML = '';
   for (let i = 0; i < text.length; i++) {
-    let b = document.createElement("char");
-    if (i == cursor)
-      b.classList = ["char cursor"];
-    else
-      b.classList = ["char"];
-    b.innerHTML = text[i] == " " ? WHITESPACE : text[i];
-    document.getElementById("textbox").appendChild(b);
+    let b = document.createElement('char');
+    if (i == cursor && text[i] != '\n') b.classList = ['char cursor'];
+    else b.classList = ['char'];
+    b.innerHTML = text[i] == '\n' ? ' ' : text[i];
+    b.innerHTML = text[i] == ' ' ? WHITESPACE : text[i];
+    if (text[i] == '\n') b.classList.add('break');
+    document.getElementById('textbox').appendChild(b);
   }
 }
 
-document.addEventListener("keypress", (ev) => {
-	let audio = document.getElementById('bg-music');
-  console.log();
-	if (ev.key == ' ') {
-		ev.preventDefault();
-		console.log('space is pressed');
-	}
-	// if (audio.paused) {
-	// 	audio.play();
-	// }
+let time = null;
+let timerId = null;
+document.addEventListener('keypress', (ev) => {
+  let modal = document.getElementById('practiceModal').style;
+  if (modal.display == 'block') return;
+
+  if (time == null) {
+    time = 0;
+    timerId = setInterval(() => {
+      time += 1000;
+    }, 1000);
+  }
+  if (ev.key == ' ') ev.preventDefault();
   if (cursor >= text.length) {
-    console.log("FINISHED!!!");
-  } else if (ev.key == text[cursor]) {
-    let curr = document.getElementsByClassName("cursor")[0];
-    curr.classList.remove("cursor");
+    console.log('FINISHED!!!');
+  } else if (
+    (ev.key === 'Enter' && text[cursor] == '\n') ||
+    ev.key == text[cursor]
+  ) {
+    let curr = document.getElementsByClassName('cursor')[0];
+    if (cursor + 1 < text.length && text[cursor + 1] != '\n')
+      curr.classList.remove('cursor');
+
     if (curr.nextElementSibling) {
-      curr.nextElementSibling.classList.add("cursor");
-      console.log(curr);
+      let sibling = $(curr).first().nextAll('.char').not('.break').first();
+      if (cursor + 1 < text.length && text[cursor + 1] != '\n')
+        sibling.addClass('cursor');
+      curr.classList.add('done');
       cursor += 1;
     } else {
-      console.log("FINISHED!!!");
+      if (timerId) clearInterval(timerId);
+      let words = text.length / 5;
+      let minute = time / 60000;
+
+      document.body.ondblclick = null;
+      let wpm = Math.round(words / minute);
+      $('.stats-container')
+        .find('.replay')
+        .on('click', function (e) {
+          e.preventDefault();
+          cursor = 0;
+          time = null;
+          bgMusic.load();
+          if (bgMusic) bgMusic.play();
+          $('.char').removeClass('done');
+          $('.char').removeClass('cursor');
+          $('.char').first().addClass('cursor');
+          $('.stats-container').hide();
+
+          document.body.ondblclick = (e) => { showStats(e) };
+        });
+
+      $('.stats-container').find('.wpm-display').text(`${wpm} wpm`);
+      $('.stats-container').find('.close-stats').hide();
+      $('.stats-container').find('.replay').show();
+      $('.stats-container').show();
+      bgMusic.pause();
+      console.log('FINISHED!!!');
     }
   }
 });
+
+let selectedAudio = new Audio(`/assets/audio/forest.mp3`);
+
+$(document).ready(function () {
+  $('.audio-selector').on('change', function (e) {
+    e.preventDefault();
+    let value = $(this).val();
+    let selected = $(this).find('option:selected')[0];
+    localStorage.setItem('audioSelected', value);
+    if (!value) {
+      if (!selectedAudio.paused) selectedAudio.pause();
+      return;
+    }
+
+    selectedAudio.load();
+    selectedAudio.src = `/assets/audio/${value}`;
+    if (selectedAudio) selectedAudio.play();
+  });
+
+  $('.audio-selector').on('blur', function (e) {
+    if (!selectedAudio.paused) selectedAudio.pause();
+  });
+  $('.audio-selector').val(localStorage.getItem('audioSelected'));
+
+  $('#themeCarousel').on('slide.bs.carousel', function (e) {
+    let activeItem = e.relatedTarget;
+    let caption = activeItem.getAttribute('data-caption');
+    $('#carouselCaption').text(caption);
+    localStorage.setItem('themeCarousel', e.to.toString());
+    $(this).find('input[name="theme"]').val(activeItem.dataset.view);
+  });
+  $('#themeCarousel').carousel({
+    pause: true,
+  });
+  $('#themeCarousel').carousel(parseInt(localStorage.getItem('themeCarousel')));
+});
+
+let bgMusic = new Audio(`/assets/audio/forest.mp3`);
+$(document).ready(function () {
+  $('#practice-menu').on('submit', function (e) {
+    e.preventDefault();
+    $('.spinner').show();
+    $('#practiceModal').hide();
+
+    document.body.ondblclick = (e) => { showStats(e) };
+
+    let textarea = $(this).find('textarea');
+    let prompt = $(this).find('input[name="prompt"]').val();
+    let capitals = $(this).find('input[name="capitals"]')[0].checked;
+    let punctuations = $(this).find('input[name="punctuations"]')[0].checked;
+    let theme = $(this).find('input[name="theme"]').val();
+    let length = $(this).find('input[name="length"]').val();
+    let audio = $(this).find('select[name="audio"]').val();
+    console.log('length:', length);
+    if (length && parseInt(length))
+      prompt = `${prompt}. And the length should not exceed ${length} words`;
+
+    console.log('prompt:', prompt);
+    console.log('audio:', audio);
+    bgMusic.load();
+    bgMusic.src = `/assets/audio/${audio}`;
+    bgMusic.loop = true;
+
+    theme = `/practice/partials/${theme}`;
+    localStorage.setItem('theme', theme);
+    fetch(theme)
+      .then((res) => {
+        if (!res.ok) return res.text().then((err) => Promise.reject(err));
+        return res.text();
+      })
+      .then((res) => {
+        // console.log(res);
+        if (res.error) return;
+        $('#theme-container').html(res);
+      });
+
+    if (textarea.val()) {
+      document.getElementById('practiceModal').style.display = 'none';
+      text = textarea.val();
+      if (!capitals) text = text.toLowerCase();
+      if (!punctuations) text = text.replace(/[^(A-Z|a-z| |\n)]/g, '');
+      beginPractice();
+      if (bgMusic) bgMusic.play();
+      return;
+    } else if (prompt == '' || !prompt) {
+      $('.spinner').hide();
+      if (!capitals) text = text.toLowerCase();
+      if (!punctuations) text = text.replace(/[^(A-Z|a-z| |\n)]/g, '');
+      beginPractice();
+      if (bgMusic) bgMusic.play();
+      return;
+    }
+
+    fetch('/practice', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ prompt }),
+    })
+      .then((res) => {
+        if (!res.ok) return res.json().then((err) => Promise.reject(err));
+        return res.json();
+      })
+      .then((res) => {
+        $('.spinner').hide();
+        document.getElementById('practiceModal').style.display = 'none';
+        text = res.response;
+        if (!capitals) text = text.toLowerCase();
+        if (!punctuations) text = text.replace(/[^(A-Z|a-z| |\n)]/g, '');
+
+        beginPractice();
+        if (bgMusic) bgMusic.play();
+      })
+      .catch((err) => {
+        $('.spinner').hide();
+        console.error(err);
+      });
+  });
+
+	function getRandomInt(min, max) {
+		const randomBuffer = new Uint32Array(1);
+		window.crypto.getRandomValues(randomBuffer);
+		let randomNumber = randomBuffer[0] / (0xFFFFFFFF + 1);
+		return Math.floor(randomNumber * (max - min) + min);
+	}
+
+  $('.generate-prompt').on('click', function (e) {
+    e.preventDefault();
+    let prompts = [
+      'a story about Ethiopia',
+      'a poem about hard working',
+      'a funny fairy tale',
+      'funny dad jokes',
+      'a horror story',
+    ];
+    let randomIndex = getRandomInt(0, prompts.length);
+    let prompt = prompts[randomIndex];
+    let index = 0;
+    $('.prompt-message').val('');
+    let intervalId = setInterval(() => {
+      if (index >= prompt.length) {
+        clearInterval(intervalId);
+        return;
+      }
+      content = $('.prompt-message').val();
+      $('.prompt-message').val(`${content}${prompt[index]}`);
+      index++;
+    }, 40);
+  });
+
+  function showStats(e) {
+    e.preventDefault();
+    let words = cursor / 5;
+    let minute = time / 60000;
+    $('.stats-container')
+      .find('.close-stats')
+      .on('click', function (e) {
+        bgMusic.play();
+        $('.stats-container').hide();
+      });
+
+    let wpm = Math.round(words / minute);
+		if (words < 5 || time < 1000	)
+    	$('.stats-container').find('.wpm-display').text(` _ wpm`);
+    else
+			$('.stats-container').find('.wpm-display').text(`${wpm} wpm`);
+    $('.stats-container').find('.close-stats').show();
+    $('.stats-container').find('.replay').hide();
+    $('.stats-container').toggle();
+    if (bgMusic.paused) bgMusic.play();
+    else bgMusic.pause();
+  }
+}); // doc ready
